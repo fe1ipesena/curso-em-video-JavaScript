28c017ae8f81053678dfdf78eb0d46fb2b6380bb
Felipe Sena <felipecsena877@gmail.com>
2023-09-15 20:39:19 -0300

    mandei

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {        //get pede ao root ("/") rotear aqui
     res.sendFile('index.html', {root: __dirname});      //server retorna mandando o index.html para o browser
                                                         //o .sendFile precisa do absolute caminho para o arquivo: https://expressjs.com/en/4x/api.html#res.sendFile 
