9217266b76d03d71c978d235b83861fef39d201d
felipe <felipecsena877@gmail.com>
2023-10-01 11:34:57 -0300

    aula5deDOM

---
+++ aula005/aula005/script/script.js
@@ -0,0 +1,20 @@
+
+var p1 = window.document.getElementsByTagName('p')[1]
+var corpo = window.document.body
+                                    /*no querySelector o # é usado para div e . para classe 
+                                    (mas é bom levar pra vida kkkk)*/
+var d = window.document.querySelector('div#msg')
+d.style.color = 'blue'
+
+/*
+var d = window.document.getElementById('msg')
+d.style.color = 'black'
+d.style.background = 'yellow'
+/d.innerText = 'estou aguardando'
+*/
+
+/*
+var d = window.document.getElementsByName('msg')[0]
+d.innerText = 'salve'
+*/
+
