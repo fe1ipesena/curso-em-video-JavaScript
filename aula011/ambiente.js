a901eaa2998f062ee6d689ea791714f37485efcf
felipe <felipecsena877@gmail.com>
2023-10-15 20:32:54 -0300

    atividadeAula11

---
+++ aula011/ambiente.js
@@ -0,0 +1,21 @@
+
+var cont = 1
+var cont2 = 11
+
+//estrutura com teste lógico primeiro e depois a execuçao
+while (cont <= 500){
+    console.log(`passo ${cont}`)
+    cont++
+}
+
+//estrutura com a execuçao e depois o teste lógico
+do{
+    console.log('n vai repetir')
+}while (cont2 <= 10)
+
+
+
+//baianagem
+/*console.log('ola senhor')
+console.log('ola senhor')
+console.log('ola senhor')*/ 
